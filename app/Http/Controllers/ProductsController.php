a991e387dea05dfa46bf484cd9143dae0692a51a
matheusmntt <matheus.monteiro7@outlook.com>
2023-06-21 00:55:26 -0300

    up

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
     public function index () {
-        return 'produtos';
+        $findProducts = Products::all();
+        
+        return view('pages.products.pagination', compact('findProducts'));
     }
 }
